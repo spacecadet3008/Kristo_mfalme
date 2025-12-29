62977964bb09f465c3b1b2c8839541cb20a999b0
spacecadet3008 <emmaleonard3008@gmail.com>
2025-12-29 17:53:37 +0300

    defaults

--- christ_king_church/settings.py
+++ christ_king_church/settings.py
@@ -92,20 +92,20 @@ WSGI_APPLICATION = 'christ_king_church.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}"""
 
-"""DATABASES = {
+DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',
         conn_max_age=600,
         conn_health_checks=True,
     )
-}"""
+}
 
 
 # Password validation
@@ -165,7 +165,7 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "media_root")
 
-"""STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'"""
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # SMS Configuration
 # Africa's Talking Configuration (for testing)
